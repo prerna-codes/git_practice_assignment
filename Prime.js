73f62e2b5f6ee67666f765f1d5588a255bd2da17
None <prernacodes@gmail.com>
2022-12-02 10:24:19 +0530

    pali&prime

---
+++ Prime.js
@@ -0,0 +1,6 @@
+let num=23
+let c=0
+for(let i=0;i<=num;i++){
+  if(num%i==0)
+   
+}
---
+++ palindrome.js
@@ -0,0 +1,11 @@
+let n = 121
+let pali = 0
+let rem
+let temp = n
+while (n != 0) {
+  
+  pali = pali * 10 + rem
+
+}
+console.log(pali)
+temp == pali ? console.log(true) : console.log(false)
